94230c4616d62bfc6c01ad5f665f5e85a6438625
mohamad <alakoum.m004@gmail.com>
2026-09-21 11:50:15 +0200

    phase 2

--- tp/back/src/main/java/org/polytech/spring/controller.java
+++ tp/back/src/main/java/org/polytech/spring/controller.java
@@ -1,16 +1,15 @@
 package org.polytech.spring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class controller {
 
     @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello";
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello World");
     }
 }
